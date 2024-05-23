3c839af97dbefc66da8460a175077a31ecfc885b
VictorFacundoO <victorfacundo122@hotmail.com>
2024-05-23 20:47:54 -0300

    atualizando

--- src/main/resources/static/JS/JsProdutos/mostrarformulario_produto.js
+++
@@ -1,7 +0,0 @@
-function mostrarFormulario(idFormulario) {
-    var formularios = document.getElementsByClassName("formulario");
-    for (var i = 0; i < formularios.length; i++) {
-        formularios[i].style.display = "none";
-    }
-    document.getElementById(idFormulario).style.display = "block";
-}
